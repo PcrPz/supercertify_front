980b8c22a099294477fe6d9c16a4696171f9d116
PcrPz <pacharaeiamsongkram@email.com>
2025-06-06 17:31:03 +0700

    feat:add Domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_URL: process.env.API_URL
-      },
-    experimental: {
-        serverActions: true,
-      },
-    images: {
-    domains: ['localhost', '29c5cfdbd24c2606c1b4e6e1a8b88eb3.r2.cloudflarestorage.com'],
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      'localhost',
+      '29c5cfdbd24c2606c1b4e6e1a8b88eb3.r2.cloudflarestorage.com',
+      'supercertifyapi.onrender.com', // ✅ เพิ่มตรงนี้
+    ],
   },
 };
 
